5ba80938812b83cd30554855e3fe970cb7a29778
Ilemar <ilemarandrade@gmail.com>
2023-05-16 16:55:00 -0400

    feat: add new traducctions

--- src/constants/traductions/en.js
+++ src/constants/traductions/en.js
@@ -19,6 +19,13 @@ const en = {
       user_exist:
         "There is already an account associated with this email or document",
     },
+    forgot_password: {
+      check_your_email:
+        "Check your email to continue with the password recovery process",
+      title_email: "Password recovery instructions",
+      success_update_password: "Password updated successfully",
+      passwords_do_not_match: "Passwords do not match",
+    },
   },
 };
 
--- src/constants/traductions/es.js
+++ src/constants/traductions/es.js
@@ -19,6 +19,13 @@ const es = {
       user_exist:
         "Ya existe una cuenta asociada a este correo electrónico o documento",
     },
+    forgot_password: {
+      check_your_email:
+        "Revisa tu correo electrónico para continuar con el proceso de recuperación de contraseña",
+      title_email: "Instrucciones para recuperación de contraseña",
+      success_update_password: "Contraseña actualizada con éxito",
+      passwords_do_not_match: "Contraseñas no coinciden",
+    },
   },
 };
 
