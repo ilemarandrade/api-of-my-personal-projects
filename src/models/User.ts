8904e610b296d6aff7a2a99398dfc547d85cfdbd
Ilemar <ilemarandrade@gmail.com>
2023-05-26 11:57:45 -0400

    refactor: User models update to typescript

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,17 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
-const User = new Schema({
+export interface IUser extends Document {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  document: string;
+  phone: string;
+  lang: string | null;
+  token_to_reset_password: string;
+}
+
+const UserSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -32,4 +43,6 @@ const User = new Schema({
   token_to_reset_password: { type: String, default: '' },
 });
 
-export default model('User', User);
+const User = model<IUser>('User', UserSchema);
+
+export default User;
