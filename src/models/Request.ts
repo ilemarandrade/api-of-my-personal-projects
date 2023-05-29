d6c19eb828a0d1fe07a206b4b9184fbab31c8dbf
Ilemar <ilemarandrade@gmail.com>
2023-05-29 11:37:05 -0400

    feat: add interface to Request

--- src/models/Request.ts
+++ src/models/Request.ts
@@ -2,6 +2,19 @@ import { Request } from 'express';
 
 export type Lang = 'es' | 'en';
 
+export interface IResponse {
+  message: string;
+}
+export interface IResponseServices<GResponse = IResponse> {
+  statusCode: number;
+  response: GResponse;
+}
+
+export interface ICommonServices {
+  user_id: number;
+  lang: Lang;
+}
+
 interface Header {
   lang?: Lang;
 }
