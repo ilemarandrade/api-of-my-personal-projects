80bb6b72029ca0e1deff1c710d04081664276f60
Ilemar <ilemarandrade@gmail.com>
2023-05-29 10:21:35 -0400

    feat: update walletControllers to typescript

--- src/controllers/walletControllers.ts
+++ src/controllers/walletControllers.ts
@@ -1,7 +1,9 @@
+import { Response } from 'express';
+import { IRequest } from '../models/Request.js';
 import walletServices from '../services/walletServices.js';
 
-const check_balance = async (req, res) => {
-  const { lang } = req.headers;
+const check_balance = async (req: IRequest, res: Response) => {
+  const { lang = 'en' } = req.headers;
   const { user } = req.user;
   const { statusCode, response } = await walletServices.check_balance({
     user_id: user._id,
@@ -11,24 +13,24 @@ const check_balance = async (req, res) => {
   return res.status(statusCode).send(response);
 };
 
-const movements = async (req, res) => {
-  const { lang } = req.headers;
+const movements = async (req: IRequest, res: Response) => {
+  const { lang = 'en' } = req.headers;
   const { user } = req.user;
   const { page, rowsPerPage, removedMoves } = req.query;
 
   const { statusCode, response } = await walletServices.movements({
     user_id: user._id,
     lang,
-    page,
-    rowsPerPage,
-    removedMoves,
+    page: Number(page) || 1,
+    rowsPerPage: Number(rowsPerPage) || 10,
+    removedMoves: removedMoves as string,
   });
 
   return res.status(statusCode).send(response);
 };
 
-const recharge = async (req, res) => {
-  const { lang } = req.headers;
+const recharge = async (req: IRequest, res: Response) => {
+  const { lang = 'en' } = req.headers;
   const payload = req.body;
   const { user } = req.user;
 
@@ -41,8 +43,8 @@ const recharge = async (req, res) => {
   return res.status(statusCode).send(response);
 };
 
-const pay = async (req, res) => {
-  const { lang } = req.headers;
+const pay = async (req: IRequest, res: Response) => {
+  const { lang = 'en' } = req.headers;
   const payload = req.body;
   const { user } = req.user;
   const { statusCode, response } = await walletServices.pay({
@@ -54,8 +56,8 @@ const pay = async (req, res) => {
   return res.status(statusCode).send(response);
 };
 
-const delete_movement = async (req, res) => {
-  const { lang } = req.headers;
+const delete_movement = async (req: IRequest, res: Response) => {
+  const { lang = 'en' } = req.headers;
   const payload = req.body;
   const { user } = req.user;
   const { statusCode, response } = await walletServices.delete_movement({
