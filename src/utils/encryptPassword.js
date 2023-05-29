c71b049427ea5823c59cd4f7ba5eb6a3192c5c67
Ilemar <ilemarandrade@gmail.com>
2023-05-29 11:52:53 -0400

    feat: update encryptPassword to ts and install encrypt @types

--- src/utils/encryptPassword.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const encrypt = async (textPlain) => {
-  return await bcrypt.hash(textPlain, 10);
-};
-
-export const compare = async (textPlain, textEncrypted) => {
-  return await bcrypt.compare(textPlain, textEncrypted);
-};
